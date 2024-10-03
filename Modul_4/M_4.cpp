dbcaf8b2094570352e2b19bfb86583d17d236722
ramapnd18 <ramatriramdhani@gmail.com>
2024-10-03 22:54:22 +0700

    update4

--- Modul_4/M_4.cpp
+++ Modul_4/M_4.cpp
@@ -2,89 +2,112 @@
 
 int main() {
 	
-//	int array[5] = {1,6,8,2,7};
-//	int panjang = sizeof(array)/sizeof(array[0]);
-//	int terbesar=array[0];
-//	int terkecil=array[0];
-//	
-//	for(int i=0; i<panjang; i++){
-//		
-//		if(array[i]<terkecil){
-//			terkecil = array[i];
-//		}
-//		if(array[i]>terbesar){
-//			terbesar = array[i];
-//		}
-//	}
-//	printf("\nAngka Terkecil %d",terkecil);
-//	printf("\nAngka Terbesar %d",terbesar);
+	printf("=======================");
+	printf("\nNama: Rama Tri Ramdhani");
+	printf("\nNPM: 247006111057");
+	printf("\nKelas: A/B");
+	printf("\n=======================\n");
 
-
-//	int angka[5]={10,20,30,40,50};
-//	angka[1]=25;	angka[3]=45;
-//	
-//	printf("{");
-//	for(int i=0; i<5; i++){
-//		printf(" %d ",angka[i]);
-//	}
-//	printf("}");
-
-	
-	
-//	int kotak[3][3]={
-//	{1,1,1},
-//	{2,2,2},
-//	{3,3,3}
-//	};
-//	int jumlah;
-//	
-//	for(int i=0;i<3;i++){
-//		for(int j=0;j<3;j++){
-//			jumlah=jumlah+kotak[i][j];		
-//		}
-//	}
-//	
-//	printf("jumlah seluruhnya %d",jumlah);
-
-
-
-	// int matrix[2][3];
-	
-	// for(int i = 0;i<2;i++){
-	// 	for(int j = 0;j<3;j++){
-	// 		printf("Masukan angka [%d][%d] : ",i,j);
-	// 		scanf("%d", &matrix[i][j]);
-	// 		getchar();
-	// 	}
-	// }
+	// int array[5] = {1,6,8,2,7};
+	// int panjang = sizeof(array)/sizeof(array[0]);
+	// int terbesar=array[0];
+	// int terkecil=array[0];
 	
 	// printf("[");
-	// for(int i = 0;i<2;i++){
-	// 	for(int j = 0;j<3;j++){
-	// 		printf(" %d ",matrix[i][j]);
+	// for(int i=0; i<panjang; i++){
+	// 	printf(" %d ",array[i]);
+	// 	if(array[i]<terkecil){
+	// 		terkecil = array[i];
 	// 	}
-	// 	if(i<1){
-	// 	printf(" \n");
-	// 	printf(" ");
+	// 	if(array[i]>terbesar){
+	// 		terbesar = array[i];
 	// 	}
 	// }
 	// printf("]");
-	
-	// printf("\n\n");
-	// printf("Transpos\n");
-	
-	// printf("[");
-	// for(int i = 0;i<3;i++){
-	// 	for(int j = 0;j<2;j++){
-	// 		printf(" %d ",matrix[j][i]);
-	// 	}
-	// 	if(i<2){
-	// 	printf(" \n");
-	// 	printf(" ");
-	// 	}
-	// }
-	// printf("]");
+	// printf("\nAngka Terkecil %d",terkecil);
+	// printf("\nAngka Terbesar %d",terbesar);
 
-	printf("Hello World!!");
+
+	// int angka[5]={10,20,30,40,50};
+	
+	// printf("Sebelum diganti\n");
+	// printf("{");
+	// for(int i=0; i<5; i++){
+	// 	printf(" %d ",angka[i]);
+	// }
+	// printf("}");
+
+	// angka[1]=25;	angka[3]=45;
+	
+	// printf("\n\nSesudah diganti\n");
+	// printf("{");
+	// for(int i=0; i<5; i++){
+	// 	printf(" %d ",angka[i]);
+	// }
+	// printf("}");
+
+	
+	
+	// int kotak[3][3]={
+	// 	{1,1,1},
+	// 	{2,2,2},
+	// 	{3,3,3}
+	// };
+	// int jumlah;
+	
+	// printf("[\n");
+	// for(int i=0;i<3;i++){
+	// 	for(int j=0;j<3;j++){
+	// 		printf(" %d ",kotak[i][j]);
+	// 		jumlah=jumlah+kotak[i][j];		
+	// 	}
+	// 	printf("\n");
+	// }
+	// printf("]\n");
+
+	// printf("jumlah seluruhnya %d",jumlah);
+
+
+
+	int matrix[2][3];
+	
+	for(int i = 0;i<2;i++){
+		for(int j = 0;j<3;j++){
+			printf("Masukan angka [%d][%d] : ",i,j);
+			scanf("%d", &matrix[i][j]);
+			getchar();
+		}
+	}
+	
+
+	printf("\nMatriks Awal\n");
+	printf("[");
+	for(int i = 0;i<2;i++){
+		for(int j = 0;j<3;j++){
+			printf(" %d ",matrix[i][j]);
+		}
+		if(i<1){
+		printf(" \n");
+		printf(" ");
+		}
+	}
+	printf("]");
+	
+	printf("\n\n");
+	printf("Hasil Transpos\n");
+	
+	printf("[");
+	for(int i = 0;i<3;i++){
+		for(int j = 0;j<2;j++){
+			printf(" %d ",matrix[j][i]);
+		}
+		if(i<2){
+		printf(" \n");
+		printf(" ");
+		}
+	}
+	printf("]");
+
+
 	return 0;
 }
